59a73f610fbc8f7efc0de3175b7759dab9ab419f
Labasse <labasse@gmail.com>
2023-01-19 11:31:56 +0100

    API calls for dynamic blazor Issue page

---
+++ ExempleSupportPortail/Client/Shared/CssToolbox.cs
@@ -0,0 +1,16 @@
+﻿using ExempleSupportPortail.Shared;
+
+namespace ExempleSupportPortail.Client.Shared
+{
+    public static class CssToolbox
+    {
+        public static string StatusCssClass(this IssueDto issue) =>
+            issue.Status switch
+            {
+                "In progress" => "warning",
+                "On hold" => "danger",
+                "Closed" => "success",
+                _ => "muted"
+            };        
+    }
+}
--- ExempleSupportPortail/Shared/IssueDto.cs
+++ ExempleSupportPortail/Shared/IssueDto.cs
@@ -2,6 +2,9 @@
 {
     public record IssueDto(int IdIssue, string User, string Area, string Status, DateTime DateCreated, DateTime? DateClosed, string Subject, string? Description, string? Comment)
     {
+        #region For deserialization only
+        public IssueDto() : this(default, "", "", "", default, null, "", null, null) { }
+        #endregion
         public IssueDto(Issue issue) : this(issue.IdIssue, issue.User.Name, issue.Area.Title, issue.Status.Title, issue.DateCreated, issue.DateClosed, issue.Subject, issue.Description, issue.Comment)
         {
         }
