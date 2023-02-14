fce71ae360bde386bf62ea6702a7641db8592c2c
Labasse <labasse@gmail.com>
2023-02-14 21:41:58 +0100

    Add auth0 to client

--- ExempleSupportPortail/Client/Program.cs
+++ ExempleSupportPortail/Client/Program.cs
@@ -8,4 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOidcAuthentication(options => builder.Configuration.Bind("Auth0", options.ProviderOptions));
+
 await builder.Build().RunAsync();
